ac36065497326aeb2c44a687319cf62358c1067e
vanderli <vderli@yahoo.com.br>
2019-03-13 10:44:20 -0300

    Criação dos testes de sessao

---
+++ src/test/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessaoTest.java
@@ -0,0 +1,58 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadorDeSessaoTest {
+	
+	private Filme rogueOne;
+	private Sala sala3D;
+	private Sessao sessaoDasDez;
+	private Sessao sessaoDasTreze;
+	private Sessao sessaoDasDezoito;
+	
+	@Before
+	public void preparaSessoes(){
+		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(180), "SCI-FI");
+		this.sala3D = new Sala("Sala 3D");
+		
+		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), sala3D, rogueOne);
+		this.sessaoDasTreze = new Sessao(LocalTime.parse("13:00:00"), sala3D, rogueOne);
+		this.sessaoDasDezoito = new Sessao(LocalTime.parse("18:00:00"), sala3D, rogueOne);
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessaoNoMesmoHorario() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		Assert.assertFalse(gerenciador.cabe(sessaoDasDez));
+	}
+	
+
+	@Test
+	public void garanteQueNaoDevePermitirSessaoTerminamdoDentroDeOutraSessao() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		Sessao sessao = new Sessao(sessaoDasDez.getHorario().minusHours(1),sala3D, rogueOne);
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		Assert.assertFalse(gerenciador.cabe(sessao));
+	}
+	
+	@Test
+	public void garanteQueNaoDevePermitirSessaoTerminamdoDentroDeSessaoExistente() {
+		List<Sessao> sessoes = Arrays.asList(sessaoDasDez);
+		Sessao sessao = new Sessao(sessaoDasDez.getHorario().plusHours(1),sala3D, rogueOne);
+		GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoes);
+		Assert.assertTrue(gerenciador.cabe(sessao));
+	}
+	
+}
